e7bae17c5b62fa1f797b15c136213fa98a6dd81c
JackMarx <seriousfools@gmail.com>
2019-01-14 11:28:26 -0600

    views

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -6,4 +6,16 @@ class Api::ExamplePagesController < ApplicationController
   def second_action
     render json: ["this", "is", "the", "second", "action"]
   end
+
+  def time_action
+    @time = Time.now.strftime("%l:%M %p")
+    render 'time_view.json.jbuilder'
+  end
 end
+
+
+
+
+
+
+
